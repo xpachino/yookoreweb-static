65a8d341ea36a81fcf64a3dfe6265f6f3db25028
xpachino <cr8ivehub@gmail.com>
2015-04-09 23:10:25 +0200

    Photo in post card
    Photo in a post card with comment field

---
+++ includes/photo-in-post.php
@@ -0,0 +1,76 @@
+<!--photo in post wrapper starts-->
+<div class="activity-stream-wrapper">
+    <div class="activity-card">
+    <!--activity header starts-->
+        <div class="activity-header">
+            <div class="content-owner-pix"><img src="img/pix.png" width="100%"></div>
+                <div class="content-owner-wrapper">
+                	<div class="content-owner-name">Paul Imisi Akpoduado</div>
+                    <div class="activity-verb">updated his status</div>
+                    <div class="status-update-timestamp">2hours ago</div>
+                </div>
+            <div class="clear"></div>
+        </div>
+   <!--activity header stops-->
+   <!--Photo display-->
+   	<div class="photo-in-post-wrapper">
+   		<div class="photo-in-status-update">
+           <a href="picture_full_view.php" style="cursor:pointer;"><img class="img-responsive" src="img/cricket.jpg"></a>
+        </div>
+    </div>
+    <!--Photo display ends-->
+    <!--Photo text display-->
+    <div class="text-content">
+     The project "Women For Change Initiative" was established in 2010 to sensitise, empower and mobilise women to participate actively in governance.
+	</div>
+   <!--Photo text display ends-->
+  <!--content stats-->
+    <div class="content-stats-wrapper">
+         <div class="content-stats">82 Likes <span style="margin:0 5px" class="glyphicon glyphicon-asterisk"></span>14 Comments</div>
+         <div class="more-content"><img src="img/icons/more.png" width="40" ></span></div>
+         <div class="clear"></div>
+    </div>
+   <!--interactin bar-->
+    <div class="interaction-bar">
+        <ul class="list-inline">
+            <li><span><img src="img/icons/like.png" width="18" ></span> Like</li>
+            <li><span><img src="img/icons/comment.png" width="18" ></span> Comment</li>
+            <li><span><img src="img/icons/share.png" width="18" ></span> Share</li>
+        </ul>
+   </div>
+   <!--comment section-->
+   <!--comment card00-->
+            <div class="comment-wrapper">
+            	<div class="comment-card">
+                	<div class="comment-owner-pix"><img src="img/pix.jpg" width="100%"></div>
+                    <div class="comment-details">
+                    	<div class="comment-owner-name">Paul Imisi</div>
+                        <div class="comment-text">She has struggled relentlessly to awaken and empower the Nigerian woman through her various summits,</div>
+                    </div>
+                <div class="clear"></div>
+                </div>
+                <div class="comment-socials"><span><img src="img/icons/like.png" width="18" ><br>15</span></div>
+            <div class="clear"></div>
+            </div>
+   <!--post comment-->
+            <div class="post-comment-wrapper">
+            	<div class="comment-owner-pix"><img src="img/pix.jpg" width="100%"></div>
+                <div class="post-comment-form-wrap">
+               	<div class="post-comment-card">
+                	 <div class="post-comment-form">
+                      <textarea rows="1" placeholder="Add a comment"></textarea>
+                	</div>
+                     <div class="add-comment-socials">
+                    <img src="img/icons/location.png" width="18" ><img src="img/icons/photo-grey.png" width="18" >
+                    </div>
+                <div style="clear:left;"></div>
+                </div>
+                <div class="press-enter">Press enter to post comment</div>
+              </div>
+            <div class="clear"></div>
+            </div>
+   <!--post comment ends--->
+            <div class="more-comments">See 25 more</div>
+ </div>
+</div>
+<!--Photo in post wrapper ends-->
