3b2ffa90511933fa590cf5beb6dea3cf4cbaeb23
xpachino <cr8ivehub@gmail.com>
2015-04-09 10:11:03 +0200

    Page Not Found - 404
    Requested page not found template

---
+++ 404.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link href="css/onboard.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/pix-modal.css" rel="stylesheet" type="text/css">
+<link href="css/photo_full_view.css" rel="stylesheet" type="text/css">
+<link href="css/profile2.css" rel="stylesheet" type="text/css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<title>Yookore - Page Not Found</title>
+<style>
+.circle {
+	border-radius: 50%;
+	width: 7px;
+	height: 7px; 
+	display:inline-block;
+	background:#999;
+	margin:0 7px 1px 7px;
+	/* width and height can be anything, as long as they're equal */
+}
+</style>
+</head>
+
+<body>
+<?php require("includes/head.php") ?>
+<!--content body starts-->
+<div class="container" style="margin-top:62px;"><!--add the margin set off the fixed header-->
+<div class="row xtra-pad">
+
+    <div style="padding:15px 20px; background:#fff; border-radius:4px; border:1px solid #ddd; margin:0 auto; width:100%; max-width:860px;">
+    <span style="font-size:18px; font-weight:700; padding:10px 0; display:block; margin-bottom:0px; border-bottom:1px solid #ddd;"><span style="margin-right:10px;"><img src="img/icons/halflings-209-alert@2x.png"></span>The page you requested was not found!</span>
+    <div style="padding:10px 0;">Hey dude, you seem to be lost or you have clicked on an expired or wrong link. Now Yookos is mad at you.  Let me advice you.</div>
+    <div style="padding:0 0 10px 0;">
+    	<ul class="list-unstyled">
+        	<li><span class="circle"></span><a href="stream.php">Go to home</a></li>
+            <li><span class="circle"></span><a href="#">Return to previous page</a></li>
+            <li><span class="circle"></span><a href="#">Get out of here quickly and stop messing around!!!</a></li>
+        </ul>
+    </div>
+    </div>
+
+</div>
+</div>
+<!--content body ends-->
+<div class="footer"></div>
+</body>
+</html>
